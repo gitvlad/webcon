488be968a5744dd1184a35178d3a5310f501da3e
Władysław Czaja <wladyslaw.czaja@gmail.com>
2025-09-17 13:20:06 +0200

    Create insert.sql

---
+++ insert.sql
@@ -0,0 +1,19 @@
+CREATE PROCEDURE dbo.usp_InsertOceny
+  @Rating       INT,
+  @Ekspercka    NUMERIC(10,1) = NULL,
+  @Pokorekcie   NUMERIC(10,1) = NULL,
+  @Podsumowanie NVARCHAR(2048) = NULL,
+  @CK01         BIT = NULL
+AS
+BEGIN
+  SET NOCOUNT ON;
+
+  INSERT INTO dbo.OCENY_
+(Rating, Ekspercka, Pokorekcie, Podsumowanie, CK01)
+  VALUES
+    (@Rating, @Ekspercka, @Pokorekcie, @Podsumowanie, @CK01);
+
+  -- Opcjonalnie zwróć ID nowego rekordu
+  SELECT SCOPE_IDENTITY() AS NewID;
+END
+GO
